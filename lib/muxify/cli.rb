a0269cbf49530923ff22a7b6b75db0e9e73fbaa2
Zubin Henner <zubin@users.noreply.github.com>
2018-01-26 06:21:31 +1100

    Note option to supply path

--- lib/muxify/cli.rb
+++ lib/muxify/cli.rb
@@ -4,12 +4,12 @@ require 'muxify/linker'
 
 module Muxify
   class CLI < Thor
-    desc "add", "Adds tmuxinator config for current path"
+    desc "add", "Adds tmuxinator config for current (or supplied) path"
     def add(root = Dir.pwd)
       Muxify::Linker.(root: root)
     end
 
-    desc "debug", "Prints tmuxinator config of current path to stdout"
+    desc "debug", "Prints tmuxinator config of current (or supplied) path to stdout"
     def debug(root = Dir.pwd)
       puts Muxify::Builder.(root: root)
     end
